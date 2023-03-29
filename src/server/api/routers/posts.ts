4be0b6ae500e71ee77d91d707db71282aaad3297
starling <starling@starlings-MBP.lan>
2023-03-29 18:38:28 -0500

    working posts

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -6,7 +6,23 @@ export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
+      orderBy: [{ createdAt: "desc" }],
     });
     return posts;
   }),
+
+  makePost: publicProcedure
+    .input(
+      z.object({
+        postBody: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      console.log(input, "from backend");
+      return await ctx.prisma.post.create({
+        data: {
+          content: input.postBody,
+        },
+      });
+    }),
 });
